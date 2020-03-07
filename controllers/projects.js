178755506e4b6c7182fcb755ea091c1f529308da
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-03-07 14:53:57 -0700

    added projects.js models

---
+++ controllers/projects.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/users');
+const Project = require('../models/projects');
+
+
+
+
+
+
+module.exports = router;
